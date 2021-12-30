2f98b83be77c449fbfd0f8a3889998816e02534f
xoniv <17964868+Niveh@users.noreply.github.com>
2021-12-31 00:41:44 +0200

    Finished intro to node.js

--- Intro To Node/index.js
+++ Intro To Node/index.js
@@ -1,1 +1,8 @@
-console.log("Hello, world!");
+import superheroes from "superheroes";
+import supervillains from "supervillains";
+
+const mySuperHero = superheroes.random();
+console.log(mySuperHero);
+
+const mySuperVillain = supervillains.random();
+console.log(mySuperVillain);
