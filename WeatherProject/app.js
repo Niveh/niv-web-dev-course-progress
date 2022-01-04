111db01e080af5f9e77f2c31143b88b2d039c199
xoniv <17964868+Niveh@users.noreply.github.com>
2022-01-05 01:14:37 +0200

    Live API Data and parsing POST requests

--- WeatherProject/app.js
+++ WeatherProject/app.js
@@ -1,11 +1,25 @@
 import express from "express";
 import https from "https";
 
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
 const app = express();
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-	const url =
-		"https://api.openweathermap.org/data/2.5/weather?q=London&appid=b310f9b6b92fc23495ceb6bd56b938c4&units=metric";
+	res.sendFile(`${__dirname}/index.html`);
+});
+
+app.post("/", (req, res) => {
+	const query = req.body.cityName;
+	const apiKey = "b310f9b6b92fc23495ceb6bd56b938c4";
+	const units = "metric";
+
+	const url = `https://api.openweathermap.org/data/2.5/weather?q=${query}&appid=${apiKey}&units=${units}`;
 
 	https.get(url, (response) => {
 		console.log(response.statusCode);
@@ -14,13 +28,17 @@ app.get("/", (req, res) => {
 			const weatherData = JSON.parse(data);
 			const temp = weatherData.main.temp;
 			const weatherDescription = weatherData.weather[0].description;
+			const icon = `http://openweathermap.org/img/wn/${weatherData.weather[0].icon}@2x.png`;
 
-			console.log(temp);
-			console.log(weatherDescription);
+			res.write(`<p>The weather is currently ${weatherDescription}</p>`);
+			res.write(
+				`<h1>The temperature in ${weatherData.name} is ${temp} degrees Celcius.</h1>`
+			);
+			res.write(`<img src="${icon}" alt="weather icon">`);
+
+			res.send();
 		});
 	});
-
-	res.send("Server is up and running.");
 });
 
 app.listen(3000, () => {
