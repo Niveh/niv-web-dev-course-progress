7d08a2240d4aee039fedb34b8b9ac6806176df33
xoniv <17964868+Niveh@users.noreply.github.com>
2022-01-07 02:12:09 +0200

    Add CSS for todolist, pass data to server

--- todolist-v1/app.js
+++ todolist-v1/app.js
@@ -9,38 +9,33 @@ const __dirname = dirname(__filename);
 const app = express();
 app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+let items = ["Buy Food", "Cook Food", "Eat Food"];
+
 app.get("/", function (req, res) {
-	const date = new Date();
-	const currentDay = date.getDay();
-	let day = "";
+	const today = new Date();
 
-	switch (currentDay) {
-		case 0:
-			day = "Sunday";
-			break;
-		case 1:
-			day = "Monday";
-			break;
-		case 2:
-			day = "Tuesday";
-			break;
-		case 3:
-			day = "Wednesday";
-			break;
-		case 4:
-			day = "Thursday";
-			break;
-		case 5:
-			day = "Friday";
-			break;
-		case 6:
-			day = "Saturday";
-			break;
-		default:
-			console.log(`Error: current day is equal to: ${currentDay}`);
-	}
+	const options = {
+		weekday: "long",
+		day: "numeric",
+		month: "long",
+	};
 
-	res.render("list", { kindOfDay: day });
+	const day = today.toLocaleDateString("en-US", options);
+
+	res.render("list", {
+		kindOfDay: day,
+		newListItems: items,
+	});
+});
+
+app.post("/", function (req, res) {
+	const item = req.body.newItem;
+	items.push(item);
+
+	res.redirect("/");
 });
 
 app.listen(3000, function () {
